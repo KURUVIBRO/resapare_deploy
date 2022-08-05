afb344084e6484a7ffbdc9a2817b3f3d8c1cb155
Aravind <aravindackal@gmail.com>
2022-08-05 19:42:26 +0530

    Host added

--- respare/urls.py
+++ respare/urls.py
@@ -20,7 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('backend.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
+    #path('__debug__/', include('debug_toolbar.urls')),
     
     
 ]
