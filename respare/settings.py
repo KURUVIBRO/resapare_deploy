53c6c07c80439a1efd67f7d23131eda2c6790fde
Aravind <aravindackal@gmail.com>
2022-08-06 00:21:27 +0530

    DB Connection

--- respare/settings.py
+++ respare/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,7 +79,7 @@ DATABASES = {
     }
 }
 
-import dj_database_url
+
 db_from_env  = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 #asdad
