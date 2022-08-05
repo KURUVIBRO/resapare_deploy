8ef34353377f97c29a9b554203479420afbdb184
Aravind <aravindackal@gmail.com>
2022-08-05 23:30:31 +0530

    SQL

--- respare/settings.py
+++ respare/settings.py
@@ -72,10 +72,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'respare.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }
+}
+
 #asdad
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -87,6 +95,7 @@ DATABASES = {
 
     }
 }
+'''
 
 '''
 DEBUG_TOOLBAR_CONFIG = {
